cfdba1fe6b19a20216a6bc5651729a7facc6ff6d
Onesinus SPT <onesinus@delman.io>
2023-01-03 21:23:16 +0700

    Add mysql and adminer container

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const mysql = require('mysql')
 
 // Constants
 const PORT = 8080;
@@ -11,6 +12,26 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/connect-db', (req, res) => {
+  const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'barbar',
+    database: 'express-db'
+  })
+
+  connection.connect()
+
+  connection.query('SELECT * FROM users', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log('The solution is: ', rows)
+  })
+
+  connection.end()
+  res.send('Connect to db')
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
